c459555c1fbbc5d1563e3221854e247a50aedecd
Gandalf <lynxdev@protonmail.com>
2023-10-17 23:11:41 +0200

    added api key

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+
+fn get_articles(url: &url){
+    let response = ureq::get(url).call()?
+}
+
 fn main() {
-    println!("Hello, Rust!");
+    let url: &str = "https://https://newsapi.org/v2/everything?q=keyword&apiKey=";
+    let articles = get_articles(url);
 }
